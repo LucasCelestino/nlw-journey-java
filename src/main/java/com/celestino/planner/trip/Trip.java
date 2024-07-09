73486c7aa12480b09a86d37f07b899f4c2ec1d22
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-09 12:13:46 -0300

    ajustando controller de viagens, criando metodo para inserção de viagens e criando payload de dados da viagem

--- src/main/java/com/celestino/planner/trip/Trip.java
+++ src/main/java/com/celestino/planner/trip/Trip.java
@@ -5,12 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Trip
 {
     @Id
@@ -34,4 +41,14 @@ public class Trip
 
     @Column(name = "owner_email", nullable = false)
     private String ownerEmail;
+
+    public Trip(TripRequestPayload data)
+    {
+        this.destination = data.destination();
+        this.startsAt = LocalDateTime.parse(data.starts_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.endsAt = LocalDateTime.parse(data.ends_at(), DateTimeFormatter.ISO_DATE_TIME);
+        this.isConfirmed = false;
+        this.ownerName = data.owner_name();
+        this.ownerEmail = data.owner_email();
+    }
 }
