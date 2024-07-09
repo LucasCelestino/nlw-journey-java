29c7166211f5604be5de486d742b42590ac9bf06
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-09 12:48:49 -0300

    adicionando metodo no controller para recuperar uma viagem

--- src/main/java/com/celestino/planner/trip/TripController.java
+++ src/main/java/com/celestino/planner/trip/TripController.java
@@ -2,10 +2,14 @@ package com.celestino.planner.trip;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
+import java.util.Optional;
 
 import com.celestino.planner.participant.ParticipantService;
 
@@ -30,5 +34,13 @@ public class TripController
         participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
 
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id)
+    {
+        Optional<Trip> trip = this.tripRepository.findById(id);
+
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
