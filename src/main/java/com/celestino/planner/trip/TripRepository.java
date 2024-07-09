c46c2bcc938e57b4e41b00becf9c17e17687647f
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-09 11:50:18 -0300

    criando repositorio para a tabela de viagens

---
+++ src/main/java/com/celestino/planner/trip/TripRepository.java
@@ -0,0 +1,7 @@
+package com.celestino.planner.trip;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TripRepository extends JpaRepository<Trip,UUID> {}
