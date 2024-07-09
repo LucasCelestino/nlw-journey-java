fdb78e2c3074498592b84f55532a5fb5a1dc4264
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-09 12:37:21 -0300

    criando service de participantes, ajustando classe entidade de viagens e finalizando metodo para criação de uma nova viagem

--- src/main/java/com/celestino/planner/trip/TripController.java
+++ src/main/java/com/celestino/planner/trip/TripController.java
@@ -1,16 +1,34 @@
 package com.celestino.planner.trip;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.celestino.planner.participant.ParticipantService;
 
 @RestController
 @RequestMapping("/trips")
 public class TripController
 {
-    public ResponseEntity<String> createTrip(@RequestBody TripRequestPayload payload)
+
+    @Autowired
+    private ParticipantService participantService;
+
+    @Autowired
+    private TripRepository tripRepository;
+
+    @PostMapping
+    public ResponseEntity<TripCreateResponse> createTrip(@RequestBody TripRequestPayload payload)
     {
         Trip newTrip = new Trip(payload);
+
+        tripRepository.save(newTrip);
+
+        participantService.registerParticipantsToEvent(payload.emails_to_invite(), newTrip.getId());
+
+        return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
     }
 }
---
+++ src/main/java/com/celestino/planner/trip/TripCreateResponse.java
@@ -0,0 +1,5 @@
+package com.celestino.planner.trip;
+
+import java.util.UUID;
+
+public record TripCreateResponse(UUID tripId) {}
