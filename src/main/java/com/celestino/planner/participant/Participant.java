b385072dcca886bf5570df504b6d2f065f0500df
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-09 15:17:50 -0300

    criando entidade para representar a tabela de participantes

---
+++ src/main/java/com/celestino/planner/participant/Participant.java
@@ -0,0 +1,41 @@
+package com.celestino.planner.participant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.UUID;
+
+import com.celestino.planner.trip.Trip;
+
+import lombok.Setter;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Participant
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private Boolean isConfirmed;
+
+    @ManyToOne
+    @Column(name = "trip_id", nullable = false)
+    private Trip trip;
+}
