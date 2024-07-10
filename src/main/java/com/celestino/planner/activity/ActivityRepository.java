9e1d39d67acc3db6edcf14ae7132d884a8fd6581
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-10 20:58:54 -0300

    criando repositorio da entidade atividades

---
+++ src/main/java/com/celestino/planner/activity/ActivityRepository.java
@@ -0,0 +1,7 @@
+package com.celestino.planner.activity;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActivityRepository extends JpaRepository<Activity,UUID> {}
