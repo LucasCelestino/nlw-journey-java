cc2fcc875b7cab8e9b27ba9e434e95d3a41c9ab6
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-11 19:41:39 -0300

    criando repositorio para a entidade link

---
+++ src/main/java/com/celestino/planner/link/LinkRepository.java
@@ -0,0 +1,6 @@
+package com.celestino.planner.link;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface LinkRepository extends JpaRepository<Link,UUID> {}
