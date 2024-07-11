1a07267dd8d941266e00081304e4366d45395dab
Lucas Celestino <fulanodetal@exemplo.br>
2024-07-11 19:39:22 -0300

    criando entidade para representar a tabela de links

---
+++ src/main/java/com/celestino/planner/link/Link.java
@@ -0,0 +1,38 @@
+package com.celestino.planner.link;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+import com.celestino.planner.trip.Trip;
+
+@Entity
+@Table(name = "links")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Link
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
